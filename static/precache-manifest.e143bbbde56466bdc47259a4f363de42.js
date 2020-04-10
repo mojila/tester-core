7fc287f865a46f235b6e6229363aae4e023f593b
Mojila <me.mojila@gmail.com>
2020-04-11 05:27:55 +0700

    init

---
+++ static/precache-manifest.e143bbbde56466bdc47259a4f363de42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235b0723907ac5bee138f56ed0747131",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05c585b1a48520762d77",
+    "url": "./static/css/main.1f96b7d5.chunk.css"
+  },
+  {
+    "revision": "32e3073b6cd6e76bf8c0",
+    "url": "./static/js/2.35ad9e98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.35ad9e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c585b1a48520762d77",
+    "url": "./static/js/main.73542e42.chunk.js"
+  },
+  {
+    "revision": "12aaab632b5a495748c3",
+    "url": "./static/js/runtime-main.2efd6a8f.js"
+  },
+  {
+    "revision": "8f3994d688ff78d91b37b91a1a6799e5",
+    "url": "./static/media/tester-logo.8f3994d6.svg"
+  }
+]);
